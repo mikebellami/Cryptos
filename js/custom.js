1ab213a4484603397e23f8129d99322ff0280691
Ogunfemi Micheal <mikebellami7@gmail.com>
2019-02-19 22:39:19 +0100

    simple landing page

---
+++ js/custom.js
@@ -0,0 +1,83 @@
+/*  Theme Name: softer - SaaS & Software Html5 Landing Page
+    Author: Saptavarana
+    Version: 1.0.0
+    Created:October 2018
+    File Description:Main JS file of the template
+*/
+
+$(window).on('scroll', function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 50) {
+        $(".sticky").addClass("stickyadd");
+    } else {
+        $(".sticky").removeClass("stickyadd");
+    }
+});
+
+$('.navbar-nav a, .scroll_down a').on('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 0
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+});
+
+$("#navbarCollapse").scrollspy({
+    offset: 20
+});
+
+$("#testi").owlCarousel({
+    autoPlay: 28000,
+    items: 3,
+    itemsDesktop: [1199, 3],
+    itemsDesktopSmall: [979, 3]
+});
+
+$('.img-zoom').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    mainClass: 'mfp-fade',
+    gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1]
+    }
+});
+
+$('.features_video').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+});
+
+$(window).on('load', function() {
+    var $container = $('.work-filter');
+    var $filter = $('#menu-filter');
+    $container.isotope({
+        filter: '*',
+        layoutMode: 'masonry',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear'
+        }
+    });
+
+    $filter.find('a').on("click", function() {
+        var selector = $(this).attr('data-filter');
+        $filter.find('a').removeClass('active');
+        $(this).addClass('active');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                animationDuration: 750,
+                easing: 'linear',
+                queue: false,
+            }
+        });
+        return false;
+    });
+});
